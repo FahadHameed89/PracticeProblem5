a2bd9ed3b07ab0434cc87d0928b45c96d1dab4ec
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-09 14:32:20 -0800

    added comments to source code to explain functions for future use

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace PracticeProblem5
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter some text and I will capitalize your text on your behalf:");
-            string userInput = Console.ReadLine();
-            string angryUser = userInput.ToUpper();
-            Console.WriteLine(angryUser);
-            Console.ReadLine();
+            string userInput = Console.ReadLine(); // This will capture whatever the user writes into a string variable known as userInput
+            string angryUser = userInput.ToUpper(); // This create a string variable named angryUSER, which is equal to the previous userInput sent through a ToUpper() command. 
+            Console.WriteLine(angryUser); // Write the capitalized text to the console. 
+            Console.ReadLine(); // And wait so the terminal window doesn't close immediately. 
         }
     }
 }
