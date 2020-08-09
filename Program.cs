2a9847b631ea19d17fdd12461a4e0b173699a235
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-09 14:29:55 -0800

    Solved problem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticeProblem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some text and I will capitalize your text on your behalf:");
+            string userInput = Console.ReadLine();
+            string angryUser = userInput.ToUpper();
+            Console.WriteLine(angryUser);
+            Console.ReadLine();
+        }
+    }
+}
